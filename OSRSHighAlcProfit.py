da11c8dc301a5cd22b71ff17d2f216ba87cee328
henrypalm <rgoodwin130@gmail.com>
2019-08-07 10:32:30 -0400

    Add files via upload
    now has a variable to store what item is being high alched, for reference purposes.

--- OSRSHighAlcProfit.py
+++ OSRSHighAlcProfit.py
@@ -1,5 +1,6 @@
 def main():
-	cost_of_item = int(input('\nWhat is the price paid for the item in question?\nEnter here: '))
+	item_name = input('\nFor refrence purposes, what are you High Alcing? (Press enter to skip)\nEnter here: ')
+	cost_of_item = int(input('What is the price paid for the item in question?\nEnter here: '))
 	High_Alc_Value = int(input('What is the High Alchemy (High Alc for short) value of the item in question?\nEnter here: '))
 	cost_Of_Nature_Rune = int(input('What is the cost of the Nature Runes used at time of purchase?\n(Average price of Nature Runes are 210)\nEnter here: '))
 
@@ -7,7 +8,8 @@ def main():
 	cost_of_business = cost_of_item + cost_Of_Nature_Rune
 	profit_margin = High_Alc_Value - cost_of_business
 	total_profit = profit_margin * volume_Of_Items
-	print("\nThe profit margin of the item in question is: " + str(profit_margin) + " gp.")
+	print("\nThe following information referes to: " + item_name)
+	print("The profit margin of the item in question is: " + str(profit_margin) + " gp.")
 	print("The total profit gained is: " + str(total_profit) + " gp.\n")
 	run_again()
 	
